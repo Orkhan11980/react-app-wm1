86d16e6b2b93601361aaf7e6b2bc8afd6ce22a01
Orkhan11980 <115529584+Orkhan11980@users.noreply.github.com>
2023-12-16 21:36:45 +0400

    directory structure ready

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
+import React from 'react';
 
 
-function App() {
+
+const App = () => {
+ 
   return (
-    <div className="App">
-     
-      
-        <a>
-      
-          Learn React
-        </a>
-
-    </div>
+    <h1>App</h1>
   );
 }
+
 
 export default App;
